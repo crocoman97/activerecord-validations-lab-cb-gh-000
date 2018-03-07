e540a6c57160ccac98cd9c99f564c18bdb5985c8
Rauof M <abdelrauof197@gmail.com>
2018-03-07 07:37:11 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+class ClickbaitValidator < ActiveModel::Validator
+  
+  def validate(record)
+    clickbait_patterns = [ /Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    unless CLICKBAIT_PATTERNS.include?(title)
+      record.errors[:name] << "not a clickbait"
+    end
+  end
+  
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_with ClickbaitValidator
 end
